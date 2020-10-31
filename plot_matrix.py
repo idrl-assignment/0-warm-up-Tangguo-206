13055b2cb2073094985632a1066135eefd328208
candytangban <tangbanllniu@163.com>
2020-10-31 19:23:22 +0800

    tangguijian

--- plot_matrix.py
+++ plot_matrix.py
@@ -7,14 +7,11 @@ def generate_random_matrix(m, n):
     return random_matrix 
 
 
-
 def save_matrix(matrix, file_name):
     plt.plot(matrix)
     plt.savefig(file_name)
     plt.show()
-    
-    
-
+        
 
 if __name__ == "__main__":
     matrix = generate_random_matrix(10, 10)
