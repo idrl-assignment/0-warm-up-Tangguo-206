42b2a3fb5ab2363d87197ef64eaec38b88456891
candytangban <tangbanllniu@163.com>
2020-10-31 19:38:08 +0800

    tangguijian

--- plot_matrix.py
+++ plot_matrix.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def generate_random_matrix(m, n):
-    random_matrix = np.random.randint(0, 2, (m, n))  # 使用numpy库函数
+    random_matrix = np.random.randint(0, 2, (m, n))
     return random_matrix
 
 
@@ -10,7 +10,7 @@ def save_matrix(matrix, file_name):
     plt.plot(matrix)
     plt.savefig(file_name)
     plt.show()
-        
+
 
 if __name__ == "__main__":
     matrix = generate_random_matrix(10, 10)
